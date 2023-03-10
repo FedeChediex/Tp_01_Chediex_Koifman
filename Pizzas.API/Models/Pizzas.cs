4b4ec34bf34a20e144cb2d2a0d6a63a1b16ff62c
MAURICIO URIEL KOIFMAN <47066568@ort>
2023-03-10 10:35:34 -0300

    api

---
+++ Pizzas.API/Models/Pizzas.cs
@@ -0,0 +1,42 @@
+
+using System.Data.SqlClient;
+using Dapper;
+using System.Collections.Generic;
+
+namespace Tp_01_Chediex_Koifman.Models;
+{
+
+    public class Pizzas
+    {
+        private string _nombre;
+        private string _descripcion;
+
+        private bool _libreGluten;
+        private float _importe;
+        
+        public string Nombre
+        {
+            get{ return _nombre; }
+            set{ _nombre = value; }
+        }
+
+        public string Descripcion
+        {
+            get{ return _descripcion; }
+            set{ _descripcion = value; }
+        }
+
+        public bool LibreGluten
+        {
+            get{ return _libreGluten; }
+            set{ _libreGluten = value; }
+        }
+
+        public float Importe
+        {
+            get{ return _importe; }
+            set{ _importe = value; }
+        }
+    }
+
+}
